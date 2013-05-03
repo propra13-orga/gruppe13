98aa1904d667beeaed998580ecdfd8b5e7ad19cf
Sebastian Derendorf <Sebastian.derendorf@hhu.de>
2013-05-03 15:37:46 +0200

    gameio veraendert

--- src/hhu/propra_2013/gruppe_13/Game_IO.java
+++ src/hhu/propra_2013/gruppe_13/Game_IO.java
@@ -3,7 +3,7 @@ package hhu.propra_2013.gruppe_13;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-class Game_IO {
+class Game_IO implements KeyListener{
 	
 	Logic logic;
 	
@@ -13,33 +13,37 @@ class Game_IO {
 			 logic.setMoveXn(true);
 			 break;
 		 case 83:
-		 }
-		 if(e.getKeyCode() == 87){ 
-			 logic.setMoveXn(true);  
-		 }
-		 if(e.getKeyCode() == 83){ 
-			 logic.setMoveX(true);  
-		 }
-		 if(e.getKeyCode() == 68){ 
+		 	 logic.setMoveX(true);  
+		 	 break;
+		 case 68: 
 			 logic.setMoveY(true);  
-		 }
-		 if(e.getKeyCode() == 65){ 
+			 break;
+		 case 65: 
 			 logic.setMoveYn(true);  
-		 }
+			 break;
 	    } 
+		 
 	 public void keyReleased(KeyEvent e) {
-		 if(e.getKeyCode() == 87){ 
+		 switch (e.getKeyCode()) {
+		 case 87: 
 			 Logic.setMoveXn(false);  
-		 }
-		 if(e.getKeyCode() == 83){ 
-			 Logic.setMoveX(false);  
-		 }
-		 if(e.getKeyCode() == 68){ 
+			 break;
+		 case 83: 
+			 Logic.setMoveX(false);
+			 break;
+		 case 68: 
 			 Logic.setMoveY(false);  
-		 }
-		 if(e.getKeyCode() == 65){ 
+			 break;
+		 case 65: 
 			 Logic.setMoveYn(false);  
+			 break;
 		 }
 	    }
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
