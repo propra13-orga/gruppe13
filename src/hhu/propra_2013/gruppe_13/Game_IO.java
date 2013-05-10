a115f0e21678ad0df2e51141e11cf110d4531a0c
Sebastian Derendorf <Sebastian.derendorf@hhu.de>
2013-05-10 19:49:18 +0200

    8 bewegungsrichtungen fertiggestellt sollte funktioniren ich schaue morgen nochmal drüber

--- src/hhu/propra_2013/gruppe_13/Game_IO.java
+++ src/hhu/propra_2013/gruppe_13/Game_IO.java
@@ -18,25 +18,25 @@ class Game_IO implements KeyEventDispatcher {
 		boolean down = false;
 		boolean left = false;
 		boolean right = false;
+		boolean upright = false;
+		boolean upleft = false;
+		boolean downright = false;
+		boolean downleft = false;
 		
 		if (e.getID() == KeyEvent.KEY_PRESSED) {
 
 			switch (e.getKeyCode()) {
 			case 87:									//87='w'
-				up =true;
-				//logic.setUp(true);
+				up = true;
 				break;
 			case 83:									//83='s'
-				down=true;
-			 	//logic.setDown(true);  
+				down = true;
 			 	break;
 			case 68: 									//68='d'
-				right=true;
-				//logic.setRight(true);  
+				right = true;
 				break;
 			case 65: 									//65='a'
-				left=true;
-//				logic.setLeft(true);  
+				left = true;
 				break;
 			case 17:									//17='ctrl'
 				logic.setPunch(true);					//wird nicht zurück auf false gestetzt kümmert sich die Logic drum.
@@ -55,33 +55,45 @@ class Game_IO implements KeyEventDispatcher {
 				break;
 				
 			}
-			if (up && !down){
-				if (left && !right){
-					logic.setUpLeft(true);
-					right = false;
-					up = false;
-				}else if (right && !left){
-					logic.setUpRight(true);
-					left = false;
-					up = false;
-				}
-			}else if (down && !up){
-				if (left && !right){
-					logic.setDownLeft(true);
-					right = false;
-					down = false;
-				}else if (right && !left){
-					logic.setDownRight(true);
-					left = false;
-					down = false;
-				}
-			}else if (!down && !up){
-				if (left && !right){
-					logic.setLeft(true);
-				}else if (right && !left){
-					logic.setRight(true);
+			if(up ^ down ^ left ^ right){
+				if (up && !down){
+					if (left && !right){
+						logic.setUpLeft(true);
+						upleft = true;
+						right = false;
+						up = false;
+					}else if (right && !left){
+						logic.setUpRight(true);
+						upright = true;
+						left = false;
+						up = false;
+					}else if (!right && !left){
+						logic.setUp(true);
+					}
+				}else if (down && !up){
+					if (left && !right){
+						logic.setDownLeft(true);
+						downleft = true;
+						right = false;
+						down = false;
+					}else if (right && !left){
+						logic.setDownRight(true);
+						downright = true;
+						left = false;
+						down = false;
+					}else if (!right && !left){
+						logic.setDown(true);
+					}
+				}else if (!down && !up){
+					if (left && !right){
+						logic.setLeft(true);
+					}else if (right && !left){
+						logic.setRight(true);
+					}
 				}
 			}
+			
+			
 		} 
 		
 		else if (e.getID() == KeyEvent.KEY_RELEASED) {
@@ -89,31 +101,73 @@ class Game_IO implements KeyEventDispatcher {
 			switch (e.getKeyCode()) {
 			case 87:									//87='w'
 				up = false;
-				//logic.setUp(false);
+				logic.setUp(false);
 				break;
 			case 83:									//83='s'
 			 	down = false;
-				//logic.setDown(false);  
+				logic.setDown(false);  
 			 	break;
 			case 68: 									//68='d'
 				right = false;
-				//logic.setRight(false);  
+				logic.setRight(false);  
 				break;
 			case 65: 									//65='a'
 				left = false;
-				//logic.setLeft(false);  
+				logic.setLeft(false);  
 				break;
-
 			}
 			
-			if(up == false)logic.setUp(false);
-			if(down == false)logic.setDown(false);
-			if(right == false)logic.setRight(false);
-			if(left == false) logic.setLeft(false);
-			if(up && right == false)logic.setUpRight(false);
-			if(up && left == false)logic.setUpLeft(false);
-			if(down && right == false)logic.setDownRight(false);
-			if(down && left == false)logic.setDownLeft(false);
+			if (upright){
+				if (!right){
+					logic.setUpRight(false);  
+					logic.setUp(true);
+					upright = false;
+					up = true;
+				}else{
+					logic.setUpRight(false);  
+					logic.setRight(true);
+					upright = false;
+					right = true;
+				}
+			}else if (upleft){
+				if (!left){
+					logic.setUpLeft(false);  
+					logic.setUp(true);
+					upleft = false;
+					up = true;
+				}else{
+					logic.setUpLeft(false);  
+					logic.setLeft(true);
+					upleft = false;
+					left = true;
+				}
+			}else if (downright){
+				if (!right){
+					logic.setDownRight(false);  
+					logic.setDown(true);
+					downright = false;
+					down = true;
+				}else{
+					logic.setDownRight(false);  
+					logic.setRight(true);
+					downright = false;
+					right = true;
+				}
+			}else if (downleft){
+				if (!left){
+					logic.setDownLeft(false);  
+					logic.setDown(true);
+					downleft = false;
+					down = true;
+				}else{
+					logic.setDownLeft(false);  
+					logic.setLeft(true);
+					downleft = false;
+					left = true;
+				}
+			}
+			
+			
 		}
 		return false;
 	}
