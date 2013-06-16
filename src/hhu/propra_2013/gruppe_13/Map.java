7e642d4e4f0af77cfe6b687c801e20bca5e063a4
Benedikt Schlosza <benedikt.schlosza@hhu.de>
2013-06-16 18:39:40 +0200

    yay-milestone

---
+++ src/hhu/propra_2013/gruppe_13/Map.java
@@ -0,0 +1,17 @@
+package hhu.propra_2013.gruppe_13;
+
+import javax.swing.JFrame;
+
+public class Map extends JFrame{
+
+	/**
+	 * @param args
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
