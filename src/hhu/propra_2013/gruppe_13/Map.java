372459f16841b5c15c5d77e695338c0fb5ff6d72
Aiko Bernehed <aiko.bernehed@hhu.de>
2013-06-17 21:06:39 +0200

    changed the Map class to something serious, as an extension of the CGO it will be drawn as just another object

--- src/hhu/propra_2013/gruppe_13/Map.java
+++ src/hhu/propra_2013/gruppe_13/Map.java
@@ -1,17 +1,73 @@
 package hhu.propra_2013.gruppe_13;
 
-import javax.swing.JFrame;
+import java.awt.Graphics2D;
 
-public class Map extends JFrame{
+class Map extends CoreGameObjects {
 
-	/**
-	 * @param args
-	 */
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*------------------------------------------------------------------------------------------------------------------------*/
+	@Override
+	int getHP() {
+		return 0;
 	}
 
+	@Override
+	double getPosX() {
+		return 0;
+	}
+
+	@Override
+	double getPosY() {
+		return 0;
+	}
+
+	@Override
+	double getRad() {
+		return 0;
+	}
+
+	@Override
+	double getVX() {
+		return 0;
+	}
+
+	@Override
+	double getVY() {
+		return 0;
+	}
+	
+	@Override
+	double getWidth() {
+		return 0;
+	}
+
+	@Override
+	double getHeight() {
+		return 0;
+	}
+
+	/*------------------------------------------------------------------------------------------------------------------------*/
+	@Override
+	void setPos(double inX, double inY) {
+	}
+
+	@Override
+	void setSpeed(double inVX, double inVY) {
+	}
+
+	@Override
+	void setRad(double inR) {
+	}
+
+	@Override
+	void setHP(int inHP) {
+	}
+
+	/*------------------------------------------------------------------------------------------------------------------------*/
+	@Override
+	void draw(Graphics2D g, int xOffset, int yOffset, double step) {
+	}
+
+	@Override
+	void takeDamage(int type, int strength) {
+	} 
 }
