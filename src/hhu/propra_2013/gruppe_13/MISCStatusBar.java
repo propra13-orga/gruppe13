852af5b5ad3a799468d0d57bfe2c4f0dc523a0e1
Aiko Bernehed <aiko.bernehed@hhu.de>
2013-06-17 11:15:29 +0200

    rewrote a couple of methods in various enemy classes and  the logic, fixed the warnings emitted by getContent in class CoreRoom

--- src/hhu/propra_2013/gruppe_13/MISCStatusBar.java
+++ src/hhu/propra_2013/gruppe_13/MISCStatusBar.java
@@ -51,7 +51,8 @@ public class MISCStatusBar {
 	void draw(Graphics2D g, int xOffset, int yOffset, double step) {
 		g.setColor(Color.BLUE);
 		this.work(step);
-	//drawing the hp++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
+		//drawing the hp++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 		int j = 1;
 			for(int i = 0 ; i < hp ; i++){
 				j = i;
